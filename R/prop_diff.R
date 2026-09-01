cfb01b2e3adb91d6d55ec928b603aed60650a55e
Wojtek <wojciech.wojciak@gmail.com>
2026-09-01 17:30:35 +0200

    Export assert_prop_data().

--- R/prop_diff.R
+++ R/prop_diff.R
@@ -389,7 +389,20 @@ estimate_proportion_diff <- function(lyt,
 #' the required conditions.
 #'
 #' @author WW
-#' @keywords internal
+#' @export
+#'
+#' @examples
+#' rsp <- c(TRUE, TRUE, FALSE, TRUE, FALSE, FALSE)
+#' grp <- factor(c(rep("Placebo", 3), rep("X", 3)))
+#' strata <- factor(c("A", "A", "B", "A", "B", "B"))
+#'
+#' assert_prop_data(rsp, grp, strata)
+#'
+#' \dontrun{
+#' # An error is raised when `grp` has only one level.
+#' grp <- factor(rep("X", 6))
+#' assert_prop_data(rsp, grp, strata)
+#' }
 assert_prop_data <- function(rsp, grp, strata = NULL) {
   checkmate::assert_logical(rsp, any.missing = FALSE)
   checkmate::assert_factor(grp, len = length(rsp), any.missing = FALSE, n.levels = 2)
